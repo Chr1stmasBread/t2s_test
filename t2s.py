6763dd2acf1d1ea0487290cd160316076bd55d75
Chr1stmasBread <93283740+Chr1stmasBread@users.noreply.github.com>
2024-04-12 18:44:26 +0300

    Create t2s.py

---
+++ t2s.py
@@ -0,0 +1,48 @@
+import requests
+import telebot
+from GPT_token import *
+# Токен вашего бота от BotFather
+TOKEN = '7062249945:AAHmQumUDijnWEFXe8pGqGNc8gp3NwqYk3M'
+
+# IAM-токен и ID папки для доступа к Yandex SpeechKit
+IAM_TOKEN = f'{iam_token}'
+FOLDER_ID = 'b1gh7qec08g3hugo7d7g'
+
+# Инициализация бота
+bot = telebot.TeleBot(TOKEN)
+
+
+# Функция для обработки команды /tts
+@bot.message_handler(commands=['tts'])
+def text_to_speech(message):
+	# Отправляем "набирает сообщение" в чат
+	bot.send_chat_action(message.chat.id, 'typing')
+
+	# Получаем текст из сообщения пользователя
+	text = ' '.join(message.text.split()[1:])
+
+	# Параметры запроса к SpeechKit
+	data = {
+		'text': text,
+		'speed': 1.1,  # Скорость речи
+		'emotion': 'good',  # Эмоциональная окраска
+		'lang': 'ru-RU',  # Язык текста (русский)
+		'voice': 'jane',  # Голос Джейн
+		'folderId': FOLDER_ID,
+	}
+	headers = {'Authorization': f'Bearer {IAM_TOKEN}'}
+
+	# Отправка запроса к SpeechKit
+	response = requests.post('https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize', headers=headers, data=data)
+
+	# Обработка ответа
+	if response.status_code == 200:
+		# Отправка аудиосообщения пользователю
+		bot.send_voice(message.chat.id, response.content)
+	else:
+		# Сообщение об ошибке, если запрос к SpeechKit завершился неудачно
+		bot.reply_to(message, "При преобразовании текста в речь возникла ошибка")
+
+
+# Запуск бота
+bot.polling()
